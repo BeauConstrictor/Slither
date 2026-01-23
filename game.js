79df45eef701adffb56fd88a1759c37676ea35b3
BeauConstrictor <scabby-gone-boring@duck.com>
2026-01-23 20:37:30 +0000

    some more perfectionism on the titile screen

--- game.js
+++ game.js
@@ -121,7 +121,7 @@ class Game {
             ctx.fillStyle = "#cdd6f4";
             ctx.textAlign = "center";
             ctx.fillText("SLITHER", centerX,
-                centerY);
+                centerY+4.5);
 
             ctx.font = `bold 24px "JetBrains Mono", monospace`;
             ctx.fillStyle = "#cdd6f4";
